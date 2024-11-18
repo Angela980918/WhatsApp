916012e122f3f67ec85d450057630274e877df6b
FinleyMadoc <87028947+FinleyMadoc@users.noreply.github.com>
2024-11-18 14:42:10 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+// import 'ant-design-vue/dist/antd';
 import {createPinia} from "pinia";
 import router from "@/router/router.js";
 
-const app = createApp();
+const app = createApp(App);
 const pinia = createPinia();
-
 
 app.use(router)
     .use(pinia)
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)), // 配置 @ 指向 src
     },
-  },
+  }
 })
